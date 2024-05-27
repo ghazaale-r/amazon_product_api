513082e6dc411d1d252a5fdaf2fa051175ffe248
ghazaale <qazale02@gmail.com>
2024-05-27 12:17:20 +0330

    add: Added ASIN validation to ProductFilter
    - Added a custom validator function to ensure ASIN format.
    - Used MaxLengthValidator and MinLengthValidator for length validation.
    - Updated ProductFilter to use these validators for the product_id field.

--- src/apps/product/api/v1/filters.py
+++ src/apps/product/api/v1/filters.py
@@ -1,11 +1,26 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxLengthValidator, MinLengthValidator, RegexValidator
+
 from django_filters import rest_framework as filters
 
 from apps.product.models import Product
 
+# ASIN is a keyword used by Amazon for its product_ids
+def validate_asin(value):
+    # "A-Z" "0-9"
+    if not value.isalnum() or not value.isupper():
+        raise ValidationError("Invalid product_id ")
 
 class ProductFilter(filters.FilterSet):
-    product_id = filters.CharFilter(field_name="product_id", required=True)
-
+    product_id = filters.CharFilter(
+        field_name="product_id", 
+        required=True, 
+        validators=[
+            MaxLengthValidator(10), 
+            MinLengthValidator(10),
+            validate_asin
+        ]
+    )
     class Meta:
         model = Product
         fields = []
